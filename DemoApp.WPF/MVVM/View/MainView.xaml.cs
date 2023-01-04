d4df496ef03a0a0e7d71e7fa3977ed129dd09454
Digi951 <ch.dieckmann@outlook.de>
2023-01-04 16:24:14 +0100

    Update navigation menu

--- DemoApp.WPF/MVVM/View/MainView.xaml.cs
+++ DemoApp.WPF/MVVM/View/MainView.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
 
 namespace DemoApp.WPF.MVVM.View;
 /// <summary>
@@ -21,5 +13,19 @@ public partial class MainView : Window
     public MainView()
     {
         InitializeComponent();
+    }
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr SendMessage(IntPtr hWnd, Int32 wMsg, Int32 wParam, IntPtr lParam);
+
+    private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        WindowInteropHelper helper = new(this);
+        SendMessage(helper.Handle, 161, 2, IntPtr.Zero);
+    }
+
+    private void pnlControlBar_MouseEnter(object sender, MouseEventArgs e)
+    {
+        this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
     }
 }
